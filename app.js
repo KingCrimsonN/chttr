00a76437699ce9419d8c7d46138667aaf6bf04e1
KingCrimsonN <MSUA1307@gmail.com>
2019-12-05 22:38:45 +0200

    Chat saving done, working database

--- app.js
+++ app.js
@@ -1,41 +1,78 @@
-
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const port = process.env.PORT || 8000;
+app.set('view engine', 'ejs');
 
-const db_url = "mongodb+srv://Mike_stavr:<password>@chttr-owvzb.mongodb.net/test?retryWrites=true&w=majority";
+const port = 8000;
 
-mongoose.connect(db_url, function(err){
-	console.log('mongodb connected', err);
+var Message = mongoose.model('Messages', {
+    username: String,
+    message: String,
+    time: String
 })
 
-app.set('view engine','ejs');
+const uri = "mongodb+srv://Mike_stavr:mi13ke07@chttr-owvzb.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, function(err) {
+    console.log("mongo connected", err);
+});
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 
 app.use(express.static('public'));
 
-app.get('/', function(req,res){
-	res.render('index');
+
+app.get('/', function(req, res) {
+    res.render('index');
 })
+
+app.get('/messages', function(req, res) {
+	console.log("get" + req);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
+})
+
+app.post("/messages", async (req, res) => {
+    try {
+        var message = new Message(req.body)
+        await message.save()
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        console.error(error)
+    }
+})
+
 
 server = app.listen(port);
 
+
+
+
+
 const io = require("socket.io")(server);
 
-io.on('connection' , function(socket){
-	console.log('New User Connected');
+io.on('connection', function(socket) {
+    console.log('New User Connected');
 
-	socket.username = "Anon";
-	socket.password = "";
+    socket.username = "Anon";
+    socket.password = "";
 
-	socket.on('sign_up', function(data){
-		socket.password = data.password;
-		socket.username = data.username;
-	})
+    socket.on('sign_up', function(data) {
+        socket.password = data.password;
+        socket.username = data.username;
+    })
 
-	socket.on('new_message',function(data){
-		io.sockets.emit('new_message', {message : data.message, time: data.time, username : socket.username});
-	})
-} )
+    socket.on('new_message', function(data) {
+        io.sockets.emit('new_message', { message: data.message, time: data.time, username: socket.username });
+    })
+})
+
+// io.on('disconection', function(socket){
+// 	console.log('User disconnected');
+// })
--- public/chat.js
+++ public/chat.js
@@ -1,55 +1,75 @@
+$(document).ready(function() {
+    var socket = io.connect();
+
+    var message = $("#message");
+    var username = $("#username");
+    var password = "";
+    var send_message = $("#send_message");
+    var send_username = $("#send_username");
+    var chatroom = $("#chatroom");
+    var time = "";
+
+    getMessages();
+
+    send_username.click(function() {
+        if ($("#password").val() == "" || $("#username").val() == "") {
+            alert("Enter both login and password first!");
+            return;
+        } else {
+            if (password == "") {
+                password = $("#password").val();
+                socket.emit('sign_up', { username: username.val(), password: password })
+            } else {
+                if (password != $("#password").val()) {
+                    alert("Wrong password");
+                } else {
+                    socket.emit('sign_up', { username: username.val() })
+                }
+            }
+        }
+        // document.getElementById('password').value = '';
+        // document.getElementById('username').value = '';
+    });
+
+    send_message.click(function() {
+        if (message.val() != '') {
+            var date = new Date();
+            var time = (date.getHours() < 10 ? '0' : '') + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+            sendMessage({ username: username.val(), message: message.val(), time: time })
+            socket.emit('new_message', {
+                message: message.val(),
+                time: time
+            })
+        }
+    })
+
+    socket.on("new_message", function(data) {
+        console.log(data);
+        addMessages(data);
+    })
+    $('.send-btn').click(function() {
+        document.getElementById('message').value = '';
+    })
+
+    function addMessages(data) {
+    chatroom.append("<p class = 'message message-text'>" + data.time + " " + data.username + ": " + data.message + "</p>");
+};
+
+function getMessages() {
+    $.get('/messages', function(data) {
+        console.log(data);
+        data.forEach(addMessages);
+    })
+        var temp = document.getElementById("chatroom");
+        $("#chatroom").scrollTop(500);
+        console.log(temp.scrollHeight);
+
+};
+
+function sendMessage(message) {
+    $.post('/messages', message);
+}
 
 
-$(function conn(){
-	var socket = io.connect();
-
-	var message = $("#message");
-	var username = $("#username");
-	var password = "";
-	var send_message = $("#send_message");
-	var send_username = $("#send_username");
-	var chatroom = $("#chatroom");
-
-	send_username.click(function(){
-		if ($("#password").val()=="" || $("#username").val()==""){
-			alert("Enter both login and password first!");
-			return;
-		}
-		else{
-			if (password==""){
-				password = $("#password").val();
-				socket.emit('sign_up', {username: username.val(), password: password})
-			}
-			else{
-				if (password!=$("#password").val()){
-					alert("Wrong password");
-				}
-				else{
-					socket.emit('sign_up', {username: username.val()})		
-				}
-			}
-		}
-	 		// document.getElementById('password').value = '';
-	 		// document.getElementById('username').value = '';
-	});
-
-	send_message.click(function(){
-		var date = new Date();
-		var time = (date.getHours() < 10 ? '0' : '') + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-		// console.log(message.val());
-		socket.emit('new_message', {
-			message: message.val(),
-			time: time
-		})
-	})
-
-	socket.on("new_message", function(data){
-		console.log(data);
-		chatroom.append("<p class = 'message message-text'>" + data.time + " " + data.username + ": " + data.message + "</p>");
-	})
-	$('.send-btn').click(function(){
-	 document.getElementById('message').value = '';
-})
 });
-
 
